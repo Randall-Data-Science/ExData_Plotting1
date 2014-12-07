6d8c34c3a66c2b16bf30d828201004337c7313d9
Randall-dsci <rjzmml+Randall-dsci@gmail.com>
2014-12-07 13:24:19 -0800

    formatting and simplification of code

--- plot4.R
+++ plot4.R
@@ -7,30 +7,30 @@ par(mfcol=c(2,2),
 
 #1
 with(df, {
-  plot(DateTime, Global_active_power, type="l", main="", 
-       xlab="", ylab="Global Active Power");
+  plot(DateTime, Global_active_power, type = "l", main = "", 
+       xlab = "", ylab = "Global Active Power");
 #   lines(DateTime, Global_active_power, lwd=1)
 })
 
 #2
 with(df, {
-    plot(DateTime, Sub_metering_1, type="l", main="", cex.lab=1.0, cex.axis=1.0, 
-         xlab="", ylab="Energy sub metering", yaxp= c(0, 30, 3));
-    lines(DateTime, Sub_metering_2, col="red");
-    lines(DateTime, Sub_metering_3, col="blue");
-    legend("topright", lty=1, col= c("black", "red", "blue"), bty = "n", 
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"));
+    plot(DateTime, Sub_metering_1, type = "l", main = "", xlab = "", 
+         ylab = "Energy sub metering");
+    lines(DateTime, Sub_metering_2, col = "red");
+    lines(DateTime, Sub_metering_3, col = "blue");
+    legend(x = "topright", lty = 1, col = c("black", "red", "blue"), bty = "n", 
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"));
 })
 
 #3
 with(df,{
-  plot(DateTime, Voltage, xlab="datetime", type="l");
+  plot(DateTime, Voltage, xlab = "datetime", type = "l");
 })
 
 #4
 with(df,{
-  plot(DateTime, Global_reactive_power, type="l",
-        xlab="datetime");
+  plot(DateTime, Global_reactive_power, type = "l",
+        xlab = "datetime");
 })
 
 dev.off()

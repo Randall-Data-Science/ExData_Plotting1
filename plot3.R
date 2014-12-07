fc39ac8376390f4c15ceecb0db0e4baec6f32668
Randall-dsci <rjzmml+Randall-dsci@gmail.com>
2014-12-07 13:05:58 -0800

    simplify and reformat code

--- plot3.R
+++ plot3.R
@@ -4,12 +4,12 @@ par(mfrow = c(1, 1))
 png(filename="plot3.png", width=480, height=480)
 
 with(df, {
-  plot(DateTime, Sub_metering_1, type="l", main="", cex.lab=1.0, cex.axis=1.0, 
-       xlab="", ylab="Energy sub metering", yaxp= c(0, 30, 3));
-  lines(DateTime, Sub_metering_2, col="red");
-  lines(DateTime, Sub_metering_3, col="blue");
-  legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1,
-         col= c("black", "red", "blue"), cex=1.0, pt.cex=1.0, bty="o");
+  plot(DateTime, Sub_metering_1, type = "l", main = "", xlab = "", 
+       ylab = "Energy sub metering");
+  lines(DateTime, Sub_metering_2, col = "red");
+  lines(DateTime, Sub_metering_3, col = "blue");
+  legend(x = "topright", lty = 1, col = c("black", "red", "blue"),
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"));
 })
 
 dev.off()
